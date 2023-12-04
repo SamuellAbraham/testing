f3ee05d8ec017b285b2d679bde2894208e5abf2b
SamuellAbraham <Samuel.Abraham@hyperisland.se>
2023-12-04 14:31:36 +0100

    correct word audio included

--- script.js
+++ script.js
@@ -5,17 +5,26 @@ const timerDisplay = document.getElementById('timer');
 let score = 0;
 let timeLeft = 90; // 90-second timer
 let wordInterval;
+let correctSound; // Variable to store the audio element for the correct sound
+
 // Generate a random word
-function generateRandomWord() {
-  const words = ['apple', 'banana', 'orange', 'grape', 'kiwi']; // Add more words here
-  const randomIndex = Math.floor(Math.random() * words.length);
-  return words[randomIndex];
+async function generateRandomWord() {
+  const url = 'https://random-word-api.herokuapp.com/word';
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    return result[0]; // Assuming the API returns an array of words, and you want the first word
+  } catch (error) {
+    console.error(error);
+  }
 }
+
 // Create a falling word
-function createFallingWord() {
+async function createFallingWord() {
   const word = document.createElement('div');
   word.classList.add('word');
-  word.textContent = generateRandomWord();
+  // Use await to get the result from the asynchronous function
+  word.textContent = await generateRandomWord();
   const startPosition = Math.random() * (wordContainer.clientWidth - 50);
   word.style.left = `${startPosition}px`;
   word.style.top = '0';
@@ -24,7 +33,7 @@ function createFallingWord() {
     { top: '0' },
     { top: '100%' }
   ], {
-    duration: 3000,
+    duration: 4000,
     iterations: 1,
     easing: 'linear'
   });
@@ -32,7 +41,19 @@ function createFallingWord() {
     word.remove();
   };
 }
-// Countdown timer function
+// Initialize the audio element
+correctSound = document.getElementById('correctSound');
+
+
+function playCorrectSound() {
+  if (correctSound) {
+    correctSound.currentTime = 0; // Reset the audio to the beginning
+    correctSound.play();
+  }
+}
+
+
+
 function countdown() {
   const timer = setInterval(() => {
     timeLeft--;
@@ -45,20 +66,38 @@ function countdown() {
     }
   }, 1000);
 }
+
 // Check user input against the displayed word
 userInput.addEventListener('input', function() {
   const currentWord = document.querySelector('.word');
-  if (currentWord && userInput.value.trim().toLowerCase() === currentWord.textContent) {
-    currentWord.remove();
-    score++;
-    scoreDisplay.textContent = score;
+  if (currentWord) {
+    const enteredWord = userInput.value.trim().toLowerCase();
+    const correctWord = currentWord.textContent.toLowerCase();
+    if (enteredWord === correctWord) {
+      // If the entered word is correct, remove the word and update the score
+      currentWord.remove();
+      score++;
+      scoreDisplay.textContent = score;
+      userInput.value = '';
+      playCorrectSound(); // Play the sound effect
+    }
+  }
+});
+
+// Additional event listener to clear input when the user misses the word
+userInput.addEventListener('change', function() {
+  const currentWord = document.querySelector('.word');
+  if (currentWord) {
+    // Clear the input when the user misses the word
     userInput.value = '';
   }
 });
+
 // Function to end the game
 function endGame() {
   alert('Game Over! Your score: ' + score);
 }
+
 // Start the timer and word generation
 countdown();
 wordInterval = setInterval(createFallingWord, 2000);
